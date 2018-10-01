8ef2d3a693a92b7baed9bb9a0531d8f507f52509
yudilevi <yudilevi@gmail.com>
2018-10-01 15:18:48 +0300

    updated formatting/naming

--- BancorConverter/BancorConverter.hpp
+++ BancorConverter/BancorConverter.hpp
@@ -10,82 +10,76 @@
 
 using namespace eosio;
 
-
-
 class BancorConverter : public eosio::contract {
+    public:
+        // @abi table 
+        struct connector {
+            account_name contract;
+            asset        currency;
+            uint64_t     weight;
+            bool         enabled;
+            uint64_t     fee;
+            account_name feeaccount;
+            uint64_t     maxfee;        
+            uint64_t     primary_key() const { return currency.symbol.name(); }
+            EOSLIB_SERIALIZE( connector, (contract)(currency)(weight)(enabled)(fee)(feeaccount)(maxfee) )
+        };
     
-    
-  
-   public:
-    // @abi table 
-    struct connector {
-        account_name contract;
-        asset        currency;
-        uint64_t     weight;
-        bool         enabled;
-        uint64_t     fee;
-        account_name feeaccount;
-        uint64_t        maxfee;        
-        uint64_t primary_key() const { return currency.symbol.name(); }
-        EOSLIB_SERIALIZE( connector, (contract)(currency)(weight)(enabled)(fee)(feeaccount)(maxfee) )
-    };
-    
-    // @abi table
-    struct cstate
-    {
-       account_name manager;
-       account_name smart_contract;
-       asset        smart_currency;
-       bool         smart_enabled;
-       bool         enabled;
-       account_name network;
-       bool         verifyram;
-       uint64_t primary_key() const { return manager; }
-       EOSLIB_SERIALIZE( cstate, (manager)(smart_contract)(smart_currency)(smart_enabled)(enabled)(network)(verifyram) )
-    };
+        // @abi table
+        struct cstate {
+            account_name manager;
+            account_name smart_contract;
+            asset        smart_currency;
+            bool         smart_enabled;
+            bool         enabled;
+            account_name network;
+            bool         verifyram;
+            uint64_t primary_key() const { return manager; }
+            EOSLIB_SERIALIZE( cstate, (manager)(smart_contract)(smart_currency)(smart_enabled)(enabled)(network)(verifyram) )
+        };
+
+        // @abi action
+        struct create {
+            account_name smart_contract;
+            asset        smart_currency;
+            bool         smart_enabled;
+            bool         enabled;
+            account_name network;
+            bool         verifyram;
+            EOSLIB_SERIALIZE( create, (smart_contract)(smart_currency)(smart_enabled)(enabled)(network)(verifyram) )
+        };
+      
+        // @abi action
+        struct setconnector {
+            account_name contract;
+            asset        currency;
+            uint64_t     weight;
+            bool         enabled;
+            uint64_t     fee;
+            account_name feeaccount;
+            uint64_t     maxfee;
+            EOSLIB_SERIALIZE( setconnector, (contract)(currency)(weight)(enabled)(fee)(feeaccount)(maxfee) )
+        };
  
-    typedef eosio::multi_index<N(cstate), cstate> converters;
-    typedef eosio::multi_index<N(connector), connector> connectors;
+        typedef eosio::multi_index<N(cstate), cstate> converters;
+        typedef eosio::multi_index<N(connector), connector> connectors;
     
-      BancorConverter( account_name self )
-       :contract(self),
-       _self(self),
-       cstates( self, self ),
-       connectors_table( self, self )
-      {}
+        BancorConverter(account_name self)
+            :contract(self),
+            _self(self),
+            cstates(self, self),
+            connectors_table(self, self)
+        {}
       
-      void apply( account_name contract, account_name act );
-      // @abi action
-      struct create
-      {
-          account_name smart_contract;
-          asset        smart_currency;
-          bool         smart_enabled;
-          bool         enabled;
-          account_name network;
-          bool         verifyram;
-          EOSLIB_SERIALIZE( create, (smart_contract)(smart_currency)(smart_enabled)(enabled)(network)(verifyram) )
-      };
-      
-      // @abi action
-      struct setconnector
-      {
-        account_name contract;
-        asset        currency;
-        uint64_t     weight;
-        bool         enabled;
-        uint64_t     fee;
-        account_name feeaccount;
-        uint64_t     maxfee;
-        EOSLIB_SERIALIZE( setconnector, (contract)(currency)(weight)(enabled)(fee)(feeaccount)(maxfee) )
-      };
-   private:
-      account_name      _self;
-      converters        cstates;
-      connectors        connectors_table;
-      const connector& lookupConnector(uint64_t name, cstate state );
-      void on( const currency::transfer& t, account_name code );
-      void convert( account_name from, eosio::asset quantity, std::string memo, account_name code);
-      void oncreate(const create& sc, account_name code );
-      void onsetconnector(const setconnector& sc, account_name code );
+        void apply(account_name contract, account_name act);
+
+    private:
+        account_name _self;
+        converters   cstates;
+        connectors   connectors_table;
+        const connector& lookupConnector(uint64_t name, cstate state );
+        void on( const currency::transfer& t, account_name code );
+        void convert( account_name from, eosio::asset quantity, std::string memo, account_name code);
+        void on_create(const create& sc, account_name code );
+        void on_set_connector(const setconnector& sc, account_name code );
 };
--- BancorNetwork/BancorNetwork.cpp
+++ BancorNetwork/BancorNetwork.cpp
@@ -3,35 +3,35 @@
 
 using namespace eosio;
 
-void BancorNetwork::on( const currency::transfer& t, account_name code ) {
-    if( t.to != _this_contract ) 
+void BancorNetwork::on(const currency::transfer& t, account_name code) {
+    if (t.to != _this_contract) 
         return;
  
     auto a = extended_asset(t.quantity, code);
-    eosio_assert( a.is_valid(), "invalid quantity in transfer" );
-    eosio_assert( a.amount != 0, "zero quantity is disallowed in transfer");
+    eosio_assert(a.is_valid(), "invalid quantity in transfer");
+    eosio_assert(a.amount != 0, "zero quantity is disallowed in transfer");
     auto memoObject = parseMemo(t.memo);
-    eosio_assert( memoObject.path.size() >= 3, "bad path format");
+    eosio_assert(memoObject.path.size() >= 3, "bad path format");
     account_name convertContract = string_to_name(memoObject.path[0].c_str());
     
     auto converter_config = converters_table.find(convertContract);
     auto token_config = tokens_table.find(code);
     
-    eosio_assert( token_config != tokens_table.end(), "unknown token" );
-    eosio_assert( converter_config != converters_table.end(), "unknown converter" );
+    eosio_assert(token_config != tokens_table.end(), "unknown token");
+    eosio_assert(converter_config != converters_table.end(), "unknown converter");
     
-    eosio_assert( token_config->enabled, "token disabled" );
-    eosio_assert( converter_config->enabled, "convertor disabled" );
+    eosio_assert(token_config->enabled, "token disabled");
+    eosio_assert(converter_config->enabled, "convertor disabled");
     
     action(
         permission_level{ _self, N(active) },
         code, N(transfer),
         std::make_tuple(_self, convertContract, t.quantity, t.memo)
-     ).send();
+    ).send();
 }
 
-void BancorNetwork::onclear(const clear& act ) {
-    require_auth( _self );
+void BancorNetwork::on_clear(const clear& act) {
+    require_auth(_self);
     auto it_converter = converters_table.begin();
     while (it_converter != converters_table.end()) {
         it_converter = converters_table.erase(it_converter);
@@ -40,58 +40,60 @@ void BancorNetwork::onclear(const clear& act ) {
     while (it_token != tokens_table.end()) {
         it_token = tokens_table.erase(it_token);
     }
-      
 }
-void BancorNetwork::onregconverter(const regconverter& sc ) {
-    require_auth( _self );
+
+void BancorNetwork::on_reg_converter(const regconverter& sc) {
+    require_auth(_self);
     auto existing = converters_table.find(sc.contract);
-    if(existing != converters_table.end()){
-        converters_table.modify(existing,_self, [&]( auto& s ) {
+    if (existing != converters_table.end()) {
+        converters_table.modify(existing,_self, [&](auto& s) {
             s.contract = sc.contract;
             s.enabled = sc.enabled;
         });
     }
-    else converters_table.emplace( _self, [&]( auto& s ) {
-            s.contract = sc.contract;
-            s.enabled = sc.enabled;
-         });
-}
-void BancorNetwork::onregtoken(const regtoken& sc ) {
-    require_auth( _self );
-    auto existing = tokens_table.find(sc.contract);
-    if(existing != tokens_table.end()){
-        tokens_table.modify(existing, _self, [&]( auto& s ) {
-            s.contract = sc.contract;
-            s.enabled = sc.enabled;
-        });
-    }
-    else tokens_table.emplace( _self, [&]( auto& s ) {
+    else converters_table.emplace(_self, [&](auto& s) {
         s.contract = sc.contract;
         s.enabled = sc.enabled;
     });
 }
 
+void BancorNetwork::on_reg_token(const regtoken& sc) {
+    require_auth(_self);
+    auto existing = tokens_table.find(sc.contract);
+    if (existing != tokens_table.end()) {
+        tokens_table.modify(existing, _self, [&](auto& s) {
+            s.contract = sc.contract;
+            s.enabled = sc.enabled;
+        });
+    }
+    else tokens_table.emplace(_self, [&](auto& s) {
+        s.contract = sc.contract;
+        s.enabled = sc.enabled;
+    });
+}
 
-void BancorNetwork::apply( account_name contract, account_name act ) {
-  if( act == N(transfer) && contract != _self) {
-     on( unpack_action_data<currency::transfer>(), contract );
-     return;
-  }
-  if( contract != _this_contract )
-     return;
-  auto& thiscontract = *this;
-  if( name{act} == N(regtoken) ) 
-    onregtoken( unpack_action_data<regtoken>() );
-  else if( name{act} == N(regconverter) ) 
-      onregconverter( unpack_action_data<regconverter>() );
-  else if (name{act} == N(clear))
-      onclear(unpack_action_data<clear>());
+void BancorNetwork::apply(account_name contract, account_name act) {
+    if (act == N(transfer) && contract != _self) {
+        on(unpack_action_data<currency::transfer>(), contract);
+        return;
+    }
+
+    if (contract != _this_contract)
+        return;
+
+    auto& thiscontract = *this;
+    if (name{act} == N(regtoken)) 
+        on_reg_token(unpack_action_data<regtoken>());
+    else if (name{act} == N(regconverter)) 
+        on_reg_converter(unpack_action_data<regconverter>());
+    else if (name{act} == N(clear))
+        on_clear(unpack_action_data<clear>());
 }
 
 extern "C" {
-    [[noreturn]] void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
-        BancorNetwork bn( receiver );
-        bn.apply( code, action );
+    [[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+        BancorNetwork bn(receiver);
+        bn.apply(code, action);
         eosio_exit(0);
     }
 }
