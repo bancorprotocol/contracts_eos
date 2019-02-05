d0a5703a4171fc7ea8dc6e3613af6827d54d718c
Yuval Weiss <yuval@bancor.network>
2019-02-05 17:31:17 +0200

    fixed invalid reserve_balance in price data events

--- test/BancorNetwork.spec.js
+++ test/BancorNetwork.spec.js
@@ -28,36 +28,43 @@ describe('BancorNetwork Contract', () => {
     const _selfopts = { authorization:[`${testUser}@active`] };
     
     it('simple convert', async function() {
-        var minReturn = 0.100;
+        const minReturn = 0.100;
+        
         const token = await _self.contract(networkToken)
-        let res = await token.transfer({ from: testUser, to: networkContract, quantity: `2.0000000000 ${networkTokenSymbol}`, memo: `1,${converter} ${tokenSymbol},${minReturn},${testUser}` }, _selfopts);
-        var events = res.processed.action_traces[0].inline_traces[2].inline_traces[1].console.split("\n");
-        // console.log(events)
+        const res = await token.transfer({ from: testUser, to: networkContract, quantity: `2.0000000000 ${networkTokenSymbol}`, memo: `1,${converter} ${tokenSymbol},${minReturn},${testUser}` }, _selfopts);
+        const events = res.processed.action_traces[0].inline_traces[2].inline_traces[1].console.split("\n");
+        
         const convertEvent = JSON.parse(events[0]);
         const priceDataEvent = JSON.parse(events[1]);
-        // assert.equal(convertEvent.return, 1.99996000, "unexpected conversion result");
+
+        assert.equal(convertEvent.return, 1.99996000, "unexpected conversion return amount");
+        assert.equal(convertEvent.conversion_fee, 0, "unexpected conversion fee");
+
         assert.equal(priceDataEvent.reserve_ratio, 0.5, "unexpected reserve_ratio");
-        // console.log("result",jObj.target_amount);
+        assert.equal(priceDataEvent.reserve_balance, 99998.00004, "unexpected reserve_balance");
     });
 
     it('2 hop convert', async function() {
         var minReturn = 0.100;
         const token = await _self.contract(tokenContract)
         let res = await token.transfer({ from: testUser, to: networkContract, quantity: `1.00000000 ${tokenSymbol}`, memo: `1,${converter} ${networkTokenSymbol} ${converter2} ${tokenSymbol2},${minReturn},${testUser}` }, _selfopts);
-        var events = res.processed.action_traces[0].inline_traces[2].inline_traces[1].console.split("\n");
-        console.log(events)
+        let events = res.processed.action_traces[0].inline_traces[2].inline_traces[1].console.split("\n");
         let convertEvent = JSON.parse(events[0]);
         let priceDataEvent = JSON.parse(events[1]);
         assert.equal(convertEvent.return, 1.0000299998, "unexpected conversion result");
+        assert.equal(convertEvent.conversion_fee, 0, "unexpected conversion fee");
+
         assert.equal(priceDataEvent.reserve_ratio, 0.5, "unexpected reserve_ratio");
+        assert.equal(priceDataEvent.reserve_balance, 100000.9999700002, "unexpected reserve_balance");
         
         events = res.processed.action_traces[0].inline_traces[2].inline_traces[2].inline_traces[2].inline_traces[1].console.split("\n");
-        console.log(events)
         convertEvent = JSON.parse(events[0]);
         priceDataEvent = JSON.parse(events[1]);
         assert.equal(convertEvent.return, 0.99802094, "unexpected conversion result");
-        assert.equal(convertEvent.conversion_fee, 0.00099951, "unexpected conversion result");
+        assert.equal(convertEvent.conversion_fee, 0.00099951, "unexpected conversion fee");
+
         assert.equal(priceDataEvent.reserve_ratio, 0.5, "unexpected reserve_ratio");
+        assert.equal(priceDataEvent.reserve_balance, 99999.00197906, "unexpected reserve_balance");
     });
 
 });
