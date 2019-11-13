69073faddf4fc3701b5896a72b7831722eee78b6
Richard Tiutiun <ricktobacco@gmail.com>
2019-11-12 22:55:46 -0500

    affiliate fee added

--- contracts/eos/BancorNetwork/BancorNetwork.cpp
+++ contracts/eos/BancorNetwork/BancorNetwork.cpp
@@ -5,41 +5,146 @@
  */
 
 #include "../Common/common.hpp"
+#include "../Token/Token.hpp"
 #include "BancorNetwork.hpp"
 
-ACTION BancorNetwork::init() {
+ACTION BancorNetwork::setmaxfee(uint64_t max_affiliate_fee) {
     require_auth(get_self());
+
+    settings settings_table(get_self(), get_self().value);
+    auto st = settings_table.find("settings"_n.value);
+    check(max_affiliate_fee > 0 && max_affiliate_fee <= MAX_FEE, "fee outside resolution");
+
+    if (st == settings_table.end())
+        settings_table.emplace(get_self(), [&](auto& s) {		
+            s.enabled = true;
+            s.max_fee = max_affiliate_fee;
+        });
+    else
+        settings_table.modify(st, same_payer, [&](auto& s) {		
+            s.max_fee = max_affiliate_fee;
+        });
+}
+
+ACTION BancorNetwork::setenabled(bool enabled) {
+    require_auth(get_self());
+
+    settings settings_table(get_self(), get_self().value);
+    auto st = settings_table.find("settings"_n.value);
+
+    check(enabled != st->enabled, "no change in 'enabled'");
+
+    if (st == settings_table.end())
+        settings_table.emplace(get_self(), [&](auto& s) {		
+            s.enabled = true;
+            s.max_fee = 0;
+        });
+    else
+        settings_table.modify(st, same_payer, [&](auto& s) {		
+            s.enabled = enabled;
+        });
 }
 
 void BancorNetwork::on_transfer(name from, name to, asset quantity, string memo) {
     // avoid unstaking and system contract ops mishaps
     if (from == get_self() || from == "eosio.ram"_n || from == "eosio.stake"_n || from == "eosio.rex"_n) 
-	    return;
+        return;
 
     check(quantity.symbol.is_valid(), "invalid quantity in transfer");
-    check(quantity.amount != 0, "zero quantity is disallowed in transfer");
-
-    auto memo_object = parse_memo(memo);
-    check(memo_object.path.size() >= 2, "bad path format");
-
-    name next_converter = memo_object.converters[0].account;
-    check(isConverter(next_converter), "converter doesn't exist");
-
-    const name destination_account = name(memo_object.dest_account.c_str());
+    check(quantity.amount > 0, "may only transfer positive quantity");
     
-    // the 'from' param must be either the destination account, or a valid converter (in case it's a "2-hop" conversion path)
-    if (from != destination_account && destination_account != BANCOR_X)
-        check(isConverter(from), "the destination account must by either the sender, or the BancorX contract account");
-    
+    settings settings_table(get_self(), get_self().value);
+    auto st = settings_table.find("settings"_n.value);
+    check(st != settings_table.end() && st->enabled, "set network settings, and 'enabled' to true");
+
+    auto memo_object = parse_memo(memo); 
+    asset new_quantity = quantity;
+
+    if (!memo_object.path.size()) { // just exited from the last conversion in the path
+        tie(new_quantity, memo_object) = pay_affiliate(from, quantity, st->max_fee, memo_object);
+        to = name(memo_object.dest_account.c_str());
+        string sender = memo_object.sender_account;
+        memo = memo_object.receiver_memo;
+
+        check(!sender.empty() && is_account(name(sender.c_str())), "invalid memo");
+        verify_entry(to, get_first_receiver(), quantity.symbol);
+        verify_min_return(new_quantity, memo_object.min_return);
+    } else {
+        auto path_size = memo_object.path.size();
+        to = memo_object.converters[0].account;
+        
+        check(path_size >= 2 && !(path_size % 2), "bad path format");
+        check(isConverter(to), "converter doesn't exist");
+
+        if (memo_object.sender_account.empty()) { // about to enter the first conversion in the path
+            memo_object.sender_account = from.to_string();
+            memo = build_memo(memo_object);
+        } else 
+            tie(new_quantity, memo_object) = pay_affiliate(from, quantity, st->max_fee, memo_object);
+    }
+    if (new_quantity.amount != quantity.amount) // if affiliate fee was deducted from was from quantity
+        memo = build_memo(memo_object);
+        
     action(
         permission_level{ get_self(), "active"_n },
         get_first_receiver(), "transfer"_n,
-        std::make_tuple(get_self(), next_converter, quantity, memo)
+        make_tuple(get_self(), to, new_quantity, memo)
     ).send();
+}
+
+tuple<asset, memo_structure> BancorNetwork::pay_affiliate(name from, asset quantity, uint64_t max_fee, memo_structure memo) {
+    if (quantity.symbol.code() == symbol_code("BNT") && !memo.affiliate_account.empty()) {
+        name affiliate = name(memo.affiliate_account);
+        uint64_t fee = stoui(memo.affiliate_fee);
+        
+        check(get_first_receiver() == BNT_TOKEN, "BNT quantity received is not authentic");
+        check(fee > 0 && max_fee > fee, "inappropriate affiliate fee");
+        check(is_account(affiliate), "affiliate is not an account");    
+
+        // double amount = quantity.amount / pow(10, quantity.symbol.precision());
+        uint64_t fee_amount = calculate_fee(quantity.amount, fee, 1); // * pow(10, quantity.symbol.precision());
+        
+        if (fee_amount > 0) {
+            asset affiliate_fee = asset(fee_amount, quantity.symbol);
+            action(
+                permission_level{ get_self(), "active"_n },
+                BNT_TOKEN, "transfer"_n,
+                make_tuple(get_self(), affiliate, affiliate_fee, string("affiliate pay"))
+            ).send();
+            EMIT_AFFILIATE_EVENT(memo.sender_account, memo.dest_account,from, 
+                                 affiliate, quantity, affiliate_fee);
+
+            string quantity_before = quantity.to_string();
+            quantity -= affiliate_fee;
+            string quantity_after = quantity.to_string();
+            //check(0, (string("..before..") + quantity_before + string("..after..") + quantity_after).c_str());
+        }
+        memo.affiliate_account = "";
+        memo.affiliate_fee = "";
+    }
+    return make_tuple(quantity, memo);
 }
 
 bool BancorNetwork::isConverter(name converter) {
     settings settings_table(converter, converter.value);
     const auto& st = settings_table.get("settings"_n.value, "settings do not exist");
     return st.enabled;
+}
+
+// asserts if a conversion resulted in an amount lower than the minimum amount defined by the caller
+void BancorNetwork::verify_min_return(asset quantity, string min_return) {
+	float ret = stof(min_return.c_str());
+    uint64_t ret_amount = ret * pow(10, quantity.symbol.precision());
+    if (ret_amount)
+        check(quantity.amount >= ret_amount, "below min return");
+    else
+        check(quantity.amount > 0, "return must be above zero");
+}
+
+// asserts if the supplied account doesn't have an entry for a given token
+void BancorNetwork::verify_entry(name account, name currency_contract, symbol currency) {
+    check(is_account(account), "destination is not an account");
+    Token::accounts accountstable(currency_contract, account.value);
+    auto ac = accountstable.find(currency.code().raw());
+    check(ac != accountstable.end(), "must have entry for token (claim token first)");
 }
--- contracts/eos/Common/common.hpp
+++ contracts/eos/Common/common.hpp
@@ -25,10 +25,13 @@ struct converter {
 
 struct memo_structure {
     path              path;
-    vector<converter> converters;   
+    vector<converter> converters;
     string            version;
     string            min_return;
     string            dest_account;
+    string            sender_account;
+    string            affiliate_account;
+    string	          affiliate_fee;
     string            receiver_memo;
 };
 
@@ -36,31 +39,30 @@ struct memo_structure {
 #define BANCOR_NETWORK "thisisbancor"_n
 #define BNT_TOKEN "bntbntbntbnt"_n
 
+constexpr static double MAX_RATIO = 1000000.0;
+constexpr static double MAX_FEE = 1000000.0;
+
 vector<string> split(const string& str, const string& delim) {
     vector<string> tokens;
     size_t prev = 0, pos = 0;
-
-    do
-    {
+    do {
         pos = str.find(delim, prev);
         if (pos == string::npos) pos = str.length();
         string token = str.substr(prev, pos-prev);
         tokens.push_back(token);
         prev = pos + delim.length();
-    }
-    while (pos < str.length() && prev < str.length());
+    } while (pos < str.length() && prev < str.length());
+
     return tokens;
 }
 
-std::string build_memo(memo_structure data) {
-    std::string pathstr = "";
-    for(auto i=0; i < data.path.size(); i++){
-        if(i != 0)
-            pathstr.append(" ");
+string build_memo(memo_structure data) {
+    string pathstr = "";
+    for (auto i = 0; i < data.path.size(); i++) {
+        if (i != 0) pathstr.append(" ");
         pathstr.append(data.path[i]);
     }
-
-    std::string memo = "";
+    string memo = "";
     memo.append(data.version);
     memo.append(",");
     memo.append(pathstr);
@@ -68,8 +70,19 @@ std::string build_memo(memo_structure data) {
     memo.append(data.min_return);
     memo.append(",");
     memo.append(data.dest_account);
+    if (!data.sender_account.empty()) {
+        memo.append(",");
+        memo.append(data.sender_account);
+    }
+    if (!data.affiliate_account.empty()) {
+        memo.append(",");
+        memo.append(data.affiliate_account);
+        memo.append(",");
+        memo.append(data.affiliate_fee);
+    }
     memo.append(";");
     memo.append(data.receiver_memo);
+
     return memo;
 }
 
@@ -78,41 +91,95 @@ std::string build_memo(memo_structure data) {
  *  e.g. - to_fixed(14.214212, 3) --> 14.214
 */
 double to_fixed(double num, int precision) {
-    return (int)(num * pow(10, precision)) / pow(10, precision);
+    return (int)(num * pow(10.0, precision)) / pow(10.0, precision);
 }
 
-memo_structure parse_memo(std::string memo) {
-    auto res = memo_structure();
-    auto split_memos = split(memo, ";"); // we separate concantenated memos with ";"
-    auto parts = split(split_memos[0], ","); // split the first memo by ","
+float stof(const char* s) {
+    float rez = 0, fact = 1;
+    
+    if (*s == '-') s++; //skip the sign
+    for (int point_seen = 0; *s; s++) {
+        if (*s == '.') {
+            if (point_seen) return 0;
+            point_seen = 1; 
+            continue;
+        }
+        int d = *s - '0';
+        if (d >= 0 && d <= 9) {
+            if (point_seen) fact /= 10.0f;
+            rez = rez * 10.0f + (float)d;
+        } else return 0;
+    }
+    return rez * fact;
+}
 
+double calculate_fee(double amount, uint64_t fee, uint8_t magnitude) {
+    return amount * (1 - pow((1 - fee / MAX_FEE), magnitude));
+}
+
+uint64_t stoui(string const& value) {
+  uint64_t result = 0;
+  size_t const length = value.size();
+  switch (length) {
+    case 7: result += (value[length -  7] - '0') * 1000000ULL;
+    case 6: result += (value[length -  6] - '0') * 100000ULL;
+    case 5: result += (value[length -  5] - '0') * 10000ULL;
+    case 4: result += (value[length -  4] - '0') * 1000ULL;
+    case 3: result += (value[length -  3] - '0') * 100ULL;
+    case 2: result += (value[length -  2] - '0') * 10ULL;
+    case 1: result += (value[length -  1] - '0');
+  }
+  return result;
+}
+
+memo_structure parse_memo(string memo) {
+    memo_structure res = memo_structure();
+    
+    vector<string> split_memos = split(memo, ";"); // we separate concantenated memos with ";"
+    vector<string> parts = split(split_memos[0], ","); // split the first memo by ","
+    
+    check(parts.size() >= 4 && parts.size() <= 7, "invalid memo");
+    
+    res.converters = {};
     res.version = parts[0];
 
     auto path_elements = split(parts[1], " ");
 
-    if (path_elements.size() == 1 && path_elements[0] == "") {
+    if (path_elements.size() == 1 && path_elements[0] == "")
         res.path = {};
-    } else
+    else
         res.path = path_elements;
-
-    res.converters = {};
+    
     for (int i = 0; i < res.path.size(); i += 2) {
         auto converter_data = split(res.path[i], ":");
-        
         auto cnvrt = converter();
         cnvrt.account = name(converter_data[0].c_str());
         cnvrt.sym = converter_data.size() > 1 ? converter_data[1] : "";
         res.converters.push_back(cnvrt);
     }
-
-    if (split_memos.size() == 2) {
+    if (split_memos.size() == 2)
         res.receiver_memo = split_memos[1];
-    } else
+    else
         res.receiver_memo = "convert"; // default memo for receiver account
 
     res.min_return = parts[2];
     res.dest_account = parts[3];
-
+    
+    // supplying an affiliate account without affiliate fee 
+    // will interpret ^account as sender of the conversion
+    if (parts.size() == 5) { // or no affiliate parts at all
+        res.sender_account = parts[4];
+    }
+    // affiliate parts present, but sender not yet set
+    else if (parts.size() == 6) { 
+        res.affiliate_account = parts[4];
+        res.affiliate_fee = parts[5];
+    }
+    // affiliate parts present, AND sender already set
+    else if (parts.size() == 7) {
+        res.sender_account = parts[4];
+        res.affiliate_account = parts[5];
+        res.affiliate_fee = parts[6];
+    }
     return res;
 }
-
