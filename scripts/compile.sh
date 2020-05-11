7f05f69d2020e4d13127a82a054f9a17c03e141e
yudilevi <yudi@bancor.network>
2020-05-12 00:49:36 +0300

    updated the compile script + builds

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-shopt -s expand_aliases
-source ~/.bash_profile
-source ~/.profile
-source ~/.bashrc
+eosiocpp() {
+    COMMAND="eosio-cpp $*"
+    docker run --rm --name eosio.cdt_v1.7.0 -v $HOME/git/contracts_eos:/project eostudio/eosio.cdt:v1.7.0 /bin/bash -c "$COMMAND"
+}
+
+PROJECT_PATH=./project
 
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-declare -a contracts=("BancorConverter" "BancorNetwork" "Token" "BancorX" "XTransferRerouter")
-for contract in "${contracts[@]}"
+for contract in "BancorConverter" "BancorNetwork" "Token" "BancorX" "XTransferRerouter"
 do
-    echo -e "${GREEN}Compiling $contract...${NC}"
-    eosio-cpp ./contracts/eos/$contract/$contract.cpp -o ./contracts/eos/$contract/$contract.wasm --abigen -I.
+    echo -e "${GREEN}Compiling $contract...${NC} "
+    eosiocpp $PROJECT_PATH/contracts/eos/$contract/$contract.cpp -o $PROJECT_PATH/contracts/eos/$contract/$contract.wasm --abigen -I.
 done
-eosio-cpp ./contracts/eos/legacy/BancorConverter/BancorConverter.cpp -o ./contracts/eos/legacy/BancorConverter/BancorConverter.wasm --abigen -I.
