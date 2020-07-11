a33bbe010d4d6dd4718e18cc1ee98fcd7be9c0a2
yudilevi <yudi@bancor.network>
2020-07-11 14:40:13 +0300

    updated token contract based on the latest eos.io token

--- contracts/eos/Token/Token.hpp
+++ contracts/eos/Token/Token.hpp
@@ -10,162 +10,148 @@ namespace eosiosystem {
 }
 
 namespace eosio {
-
    using std::string;
 
+   /**
+    * eosio.token contract (with addition of `transferbyid`) defines the structures and actions that allow users to create, issue, and manage
+    * tokens on EOSIO based blockchains.
+    */
+    class [[eosio::contract]] Token : public contract {
+        public:
+            using contract::contract;
 
-    /**
-     * @defgroup BancorToken Token
-     * @brief BNT Token contract
-     * @details based on `eosio.token` contract (with addition of `transferbyid`) defines the
-     * structures and actions that allow to create, issue, and transfer BNT tokens
-     * @{
-     */
-   CONTRACT Token : public contract {
-      public:
-         using contract::contract;
+            /**
+             * Allows `issuer` account to create a token in supply of `maximum_supply`. If validation is successful a new entry in statstable for token symbol scope gets created.
+             *
+             * @param issuer - the account that creates the token,
+             * @param maximum_supply - the maximum supply set for the token created.
+             *
+             * @pre Token symbol has to be valid,
+             * @pre Token symbol must not be already created,
+             * @pre maximum_supply has to be smaller than the maximum supply allowed by the system: 1^62 - 1.
+             * @pre Maximum supply must be positive;
+             */
+            [[eosio::action]]
+            void create(const name& issuer, const asset& maximum_supply);
+            /**
+             *  This action issues to `to` account a `quantity` of tokens.
+             *
+             * @param to - the account to issue tokens to, it must be the same as the issuer,
+             * @param quntity - the amount of tokens to be issued,
+             * @memo - the memo string that accompanies the token issue transaction.
+             */
+            [[eosio::action]]
+            void issue(const name& to, const asset& quantity, const string& memo);
 
-         struct [[eosio::table]] account {
-            asset    balance;
+            /**
+             * The opposite for create action, if all validations succeed,
+             * it debits the statstable.supply amount.
+             *
+             * @param quantity - the quantity of tokens to retire,
+             * @param memo - the memo string to accompany the transaction.
+             */
+            [[eosio::action]]
+            void retire(const asset& quantity, const string& memo);
 
-            uint64_t primary_key()const { return balance.symbol.code().raw(); }
-         };
+            /**
+             * Allows `from` account to transfer to `to` account the `quantity` tokens.
+             * One account is debited and the other is credited with quantity tokens.
+             *
+             * @param from - the account to transfer from,
+             * @param to - the account to be transferred to,
+             * @param quantity - the quantity of tokens to be transferred,
+             * @param memo - the memo string to accompany the transaction.
+             */
+            [[eosio::action]]
+            void transfer(const name& from, const name& to, const asset& quantity, const string& memo);
 
-        struct [[eosio::table]] currency_stats {
-            asset    supply;
-            asset    max_supply;
-            name     issuer;
+            /**
+             * @brief used for cross chain transfers
+             * @param from - sender of the amount should match target
+             * @param to - receiver of the amount
+             * @param amount_account - scope (target) of the transfer
+             * @param amount_id - id of the intended transfer, containing amount
+             * @param memo - memo for the transfer
+             */
+            [[eosio::action]]
+            void transferbyid(const name& from, const name& to, const name& amount_account, uint64_t amount_id, const string& memo);
 
-            uint64_t primary_key()const { return supply.symbol.code().raw(); }
-        };
+            /**
+             * Allows `ram_payer` to create an account `owner` with zero balance for
+             * token `symbol` at the expense of `ram_payer`.
+             *
+             * @param owner - the account to be created,
+             * @param symbol - the token to be payed with by `ram_payer`,
+             * @param ram_payer - the account that supports the cost of this action.
+             *
+             * More information can be read [here](https://github.com/EOSIO/eosio.contracts/issues/62)
+             * and [here](https://github.com/EOSIO/eosio.contracts/issues/61).
+             */
+            [[eosio::action]]
+            void open(const name& owner, const symbol& symbol, const name& ram_payer);
 
+            /**
+             * This action is the opposite for open, it closes the account `owner`
+             * for token `symbol`.
+             *
+             * @param owner - the owner account to execute the close action for,
+             * @param symbol - the symbol of the token to execute the close action for.
+             *
+             * @pre The pair of owner plus symbol has to exist otherwise no action is executed,
+             * @pre If the pair of owner plus symbol exists, the balance has to be zero.
+             */
+            [[eosio::action]]
+            void close(const name& owner, const symbol& symbol);
 
-        struct [[eosio::table]] amounts_t {
-            uint64_t custom_id;
-            name target;
-            asset quantity;
-            uint64_t primary_key() const { return custom_id; }
-        };
+            static asset get_supply(const name& token_contract_account, const symbol_code& sym_code)
+            {
+                stats statstable(token_contract_account, sym_code.raw());
+                const auto& st = statstable.get(sym_code.raw());
+                return st.supply;
+            }
 
-        typedef eosio::multi_index<"amounts"_n, amounts_t> amounts;
+            static asset get_balance(const name& token_contract_account, const name& owner, const symbol_code& sym_code)
+            {
+                accounts accountstable(token_contract_account, owner.value);
+                const auto& ac = accountstable.get(sym_code.raw());
+                return ac.balance;
+            }
 
-         typedef eosio::multi_index< "accounts"_n, account > accounts;
-         typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+            using create_action = eosio::action_wrapper<"create"_n, &Token::create>;
+            using issue_action = eosio::action_wrapper<"issue"_n, &Token::issue>;
+            using retire_action = eosio::action_wrapper<"retire"_n, &Token::retire>;
+            using transfer_action = eosio::action_wrapper<"transfer"_n, &Token::transfer>;
+            using open_action = eosio::action_wrapper<"open"_n, &Token::open>;
+            using close_action = eosio::action_wrapper<"close"_n, &Token::close>;
 
-         /**
-          * Allows `issuer` account to create a token in supply of `maximum_supply`. If validation is successful a new entry in statstable for token symbol scope gets created.
-          *
-          * @param issuer - the account that creates the token,
-          * @param maximum_supply - the maximum supply set for the token created.
-          *
-          * @pre Token symbol has to be valid,
-          * @pre Token symbol must not be already created,
-          * @pre maximum_supply has to be smaller than the maximum supply allowed by the system: 1^62 - 1.
-          * @pre Maximum supply must be positive;
-          */
-         [[eosio::action]]
-         void create( const name&   issuer,
-                      const asset&  maximum_supply);
-         /**
-          *  This action issues to `to` account a `quantity` of tokens.
-          *
-          * @param to - the account to issue tokens to, it must be the same as the issuer,
-          * @param quntity - the amount of tokens to be issued,
-          * @memo - the memo string that accompanies the token issue transaction.
-          */
-         [[eosio::action]]
-         void issue( const name& to, const asset& quantity, const string& memo );
+            struct [[eosio::table]] account {
+                asset    balance;
 
-         /**
-          * The opposite for create action, if all validations succeed,
-          * it debits the statstable.supply amount.
-          *
-          * @param quantity - the quantity of tokens to retire,
-          * @param memo - the memo string to accompany the transaction.
-          */
-         [[eosio::action]]
-         void retire( const asset& quantity, const string& memo );
+                uint64_t primary_key()const { return balance.symbol.code().raw(); }
+            };
 
-         /**
-          * Allows `from` account to transfer to `to` account the `quantity` tokens.
-          * One account is debited and the other is credited with quantity tokens.
-          *
-          * @param from - the account to transfer from,
-          * @param to - the account to be transferred to,
-          * @param quantity - the quantity of tokens to be transferred,
-          * @param memo - the memo string to accompany the transaction.
-          */
-         [[eosio::action]]
-         void transfer( const name&    from,
-                        const name&    to,
-                        const asset&   quantity,
-                        const string&  memo );
+            struct [[eosio::table]] currency_stats {
+                asset    supply;
+                asset    max_supply;
+                name     issuer;
 
-        /**
-         * @brief used for cross chain transfers
-         * @param from - sender of the amount should match target
-         * @param to - receiver of the amount
-         * @param amount_account - scope (target) of the transfer
-         * @param amount_id - id of the intended transfer, containing amount
-         * @param memo - memo for the transfer
-         */
-        [[eosio::action]]
-        void transferbyid( const name& from,
-                           const name& to,
-                           const name& amount_account,
-                           uint64_t amount_id,
-                           const string& memo );
+                uint64_t primary_key()const { return supply.symbol.code().raw(); }
+            };
 
-         /**
-          * Allows `ram_payer` to create an account `owner` with zero balance for
-          * token `symbol` at the expense of `ram_payer`.
-          *
-          * @param owner - the account to be created,
-          * @param symbol - the token to be payed with by `ram_payer`,
-          * @param ram_payer - the account that supports the cost of this action.
-          *
-          * More information can be read [here](https://github.com/EOSIO/eosio.contracts/issues/62)
-          * and [here](https://github.com/EOSIO/eosio.contracts/issues/61).
-          */
-         [[eosio::action]]
-         void open( const name& owner, const symbol& symbol, const name& ram_payer );
+            struct [[eosio::table]] amounts_t {
+                uint64_t custom_id;
+                name target;
+                asset quantity;
 
-         /**
-          * This action is the opposite for open, it closes the account `owner`
-          * for token `symbol`.
-          *
-          * @param owner - the owner account to execute the close action for,
-          * @param symbol - the symbol of the token to execute the close action for.
-          *
-          * @pre The pair of owner plus symbol has to exist otherwise no action is executed,
-          * @pre If the pair of owner plus symbol exists, the balance has to be zero.
-          */
-         [[eosio::action]]
-         void close( const name& owner, const symbol& symbol );
+                uint64_t primary_key() const { return custom_id; }
+            };
 
-         static asset get_supply( const name& token_contract_account, const symbol_code& sym_code )
-         {
-            stats statstable( token_contract_account, sym_code.raw() );
-            const auto& st = statstable.get( sym_code.raw() );
-            return st.supply;
-         }
+            typedef eosio::multi_index<"amounts"_n, amounts_t> amounts;
+            typedef eosio::multi_index< "accounts"_n, account > accounts;
+            typedef eosio::multi_index< "stat"_n, currency_stats > stats;
 
-         static asset get_balance( const name& token_contract_account, const name& owner, const symbol_code& sym_code )
-         {
-            accounts accountstable( token_contract_account, owner.value );
-            const auto& ac = accountstable.get( sym_code.raw() );
-            return ac.balance;
-         }
-
-         using create_action = eosio::action_wrapper<"create"_n, &Token::create>;
-         using issue_action = eosio::action_wrapper<"issue"_n, &Token::issue>;
-         using retire_action = eosio::action_wrapper<"retire"_n, &Token::retire>;
-         using transfer_action = eosio::action_wrapper<"transfer"_n, &Token::transfer>;
-         using open_action = eosio::action_wrapper<"open"_n, &Token::open>;
-         using close_action = eosio::action_wrapper<"close"_n, &Token::close>;
-      private:
-         void sub_balance( const name& owner, const asset& value );
-         void add_balance( const name& owner, const asset& value, const name& ram_payer );
-   };
-
+        private:
+            void sub_balance(const name& owner, const asset& value);
+            void add_balance(const name& owner, const asset& value, const name& ram_payer);
+    };
 }
